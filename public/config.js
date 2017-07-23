7507ee665b4b506ed4b0097c7b03bd5f5903075b
Ismaïl <nguyen.ismail@gmail.com>
2017-07-24 00:42:52 +0200

    add config file to change according to user's credential

---
+++ public/config.js
@@ -0,0 +1,7 @@
+var firebaseConfig = {
+    apiKey: "See on Firebase Console",
+    authDomain: "See on Firebase Console",
+    databaseURL: "See on Firebase Console",
+    storageBucket: "See on Firebase Console",
+    messagingSenderId: "See on Firebase Console"
+};
