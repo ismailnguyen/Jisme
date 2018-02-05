472826b85ee8f6eee764e4cea0e208791f380578
Ismaïl <nguyen.ismail@gmail.com>
2018-02-05 21:03:54 +0100

    Add Material Design lite

--- public/src/components/App.vue
+++ public/src/components/App.vue
@@ -3,3 +3,11 @@
         <router-view></router-view>
     </div>
 </template>
+
+<script>
+  require('material-design-lite')
+</script>
+<style>
+  @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+  @import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
+</style>
