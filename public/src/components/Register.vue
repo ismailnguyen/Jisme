bad65cc69a1b160ad8719af3b3ddedb8dd00a49f
Ismaïl <nguyen.ismail@gmail.com>
2018-02-11 19:36:01 +0100

    Update registration component

--- public/src/components/Register.vue
+++ public/src/components/Register.vue
@@ -6,12 +6,12 @@
             <input type="text" placeholder="email" class="form-control" v-model="email">
             <input type="password" placeholder="password" class="form-control" v-model="password">
             <br/>
-            <button class="btn btn-primary" @click="signUp">Sign Up</button>
+            <button class="ce pi" @click="signUp">Sign Up</button>
         </div>
         <br/>
         <p>{{error.message}}</p>
         <br/>
-        <router-link to="/signin">Already an user ? Sign in</router-link>
+        <router-link to="/login">Already an user ? Sign in</router-link>
     </div>
 </template>
 
@@ -29,6 +29,7 @@
         },
         methods: {
             signUp() {
+                this.error.message = 'Error : Functionnality non available';
             }
         }
     }
