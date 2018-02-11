d8e41b9326a7eeb1e02ff4a9317f417bdbe869b7
Ismaïl <nguyen.ismail@gmail.com>
2018-02-11 19:35:44 +0100

    Add app component

--- public/src/components/App.vue
+++ public/src/components/App.vue
@@ -1,13 +1,5 @@
 <template>
-    <div id="app">
+    <div id="app" class="bw">
         <router-view></router-view>
     </div>
 </template>
-
-<script>
-  require('material-design-lite')
-</script>
-<style>
-  @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
-  @import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
-</style>
