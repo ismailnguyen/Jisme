2bf4d86b1c54d75280119051d170b6e36c45b4ab
Ismaïl <nguyen.ismail@gmail.com>
2018-02-11 19:35:35 +0100

    Add  Dashboard component

--- public/src/components/Dashboard.vue
+++ public/src/components/Dashboard.vue
@@ -1,71 +1,209 @@
 <template>
-    <div>
-        <h3>Jisme</h3>
+    <div class="di">   
+        <div class="em brh">
+            <nav class="bro">
+                <div class="bri">
+                    <button class="bqe bqg brj" type="button" data-toggle="collapse" data-target="#nav-collapse">
+                        <span class="aep">Toggle nav</span>
+                    </button>
+                    <a class="brk bsi" href="./">
+                        <span class="bv brl"><img src="/images/touch/favicon64.png"/></span>
+                        <span class="bv brl">Jisme</span>
+                    </a>
+                </div>
 
-        <input type="text" v-model="search" />
-        <button class="btn btn-danger btn-sm signout-btn" @click="signOut">Sign Out</button>
+                <form class="brm">
+                    <input class="form-control" type="text" v-model="search" id="search" placeholder="Search..." />
+                    <button class="po">
+                        <span class="bv bje"></span>
+                    </button>
+                </form>
 
-        <hr>
-        <AddAccount />
-        <hr>
+                <div class="collapse bql" id="nav-collapse">
+                    <ul class="nav qq nav-stacked xx">
+                        <li class="ayx">Tags</li>
 
-        <div class="col-md-12">
-            <Account v-for="(account, index) in filteredAccounts" v-bind:key="index" :account="account" :query="query" />
+                        <li class="qp">
+                            <router-link :to="{ name: 'Dashboard' }" class="qn" v-bind:class="{ 'active': currentTag == 'All' }">All</router-link>                            
+                        </li>
+                        
+                        <li class="qp" v-for="(tag, index) in sortedTags" v-bind:key="index">
+                            <router-link :to="{ name: 'Tag', params: { tag: tag } }" class="qn" v-bind:class="{ 'active': currentTag == tag }">{{ tag }}</router-link>
+                        </li>
+
+                        <li class="ayx">
+                            <hr class="bsj afx">
+                        </li>
+                        
+                        <li class="qp">
+                            <button class="ce tb" @click="signOut">Sign out</button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
+        
+        <div class="es bsk" v-show="user">
 
+            <button @click="addAccount" v-show="!showAddAccountForm" class="ce pb ahr floating-btn">New</button>
+            <button @click="cancelAddAccount" v-show="showAddAccountForm" class="ce tb ahr floating-btn">Cancel</button>
+
+            <div class="brv">
+                <div class="brw">
+                    <h6 class="bry">{{ user.email }}</h6>
+                    <h2 class="brx">{{ currentTag }}</h2>
+                </div>
+
+                <div class="qb brz">
+                    <div class="ayt brg">
+                        <datepicker id="date" v-model="currentDate"></datepicker>
+                        <span class="bv bck"></span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="nu">
+
+                <table class="ck">
+                    
+                    <AddAccount v-show="showAddAccountForm" :user="user" @isFinished="onAccountAdd" />
+                
+                    <tr>
+                        <th>Platform</th>
+                        <th>Login</th>
+                        <th>Action</th>
+                    </tr>
+
+                    <Account v-for="(account, index) in sortedAccounts" v-bind:key="index" :account="account" :tag="currentTag" :date="currentDate" :query="search" :user="user" />
+                    
+                </table>
+
+                <hr class="bsj afx">
+                
+                &copy; 2018 Jisme
+            
+            </div>
+        </div>
     </div>
+    <!-- <div>
+        <h2 class="mdc-typography--display2">Jisme</h2>
+        
+        <div class="mdc-text-field" data-mdc-auto-init="MDCTextField">
+            <input type="text" class="mdc-text-field__input" id="search-input" v-model="search" />
+            <label for="search-input" class="mdc-text-field__label">Search...</label>
+        </div>
+    </div> -->
 </template>
 
 <script>
-    import { logout, getUser } from '../utils/auth'
+    import { getUser } from '../utils/auth'
+    import { sortByName } from '../utils/sort'
     import AccountsService from '../services/AccountsService'
     import AddAccount from './AddAccount.vue'
     import Account from './Account.vue'
+    import Datepicker from 'vuejs-datepicker';
     
     export default {
         data()
         {
             return {
-                accounts: [],
-                search: ''
+                user: getUser(),
+                search: '',
+                currentDate: new Date(),
+                showAddAccountForm: false
             }
         },
         methods: {
-            signOut() {
-                logout();
+            signOut: function () {
+                let userService = new UserService();
+
+                userService.logout();
             },
-            getDecryptedAccount(account, token)
+            cancelAddAccount: function () {
+                this.showAddAccountForm = false;
+            },
+            addAccount: function () {
+                this.showAddAccountForm = true;
+            },
+            onAccountAdd: function (isFinished) {
+                this.showAddAccountForm = !isFinished;
+            },
+            fetchAccounts: function ()
             {
-                var encryptedAccount = JSON.parse(JSON.stringify(account)); // Clone object without reference
-            
-                encryptedAccount['platform'] = decrypt(account.platform, token);
-                encryptedAccount['login'] = decrypt(account.login, token);
-                encryptedAccount['password'] = decrypt(account.password, token);
-                encryptedAccount['tags'] = decrypt(account.tags, token);
-            
-                return encryptedAccount;
+                let accountsService = new AccountsService(this.user, this.$store.state);
+
+                accountsService.get()
+                .then(this.setOldestDate);
+            },
+            setOldestDate: function (accounts)
+            {
+                accounts.forEach(account =>
+                {
+                    let createdDate = new Date(account.created_date);
+                    console
+                    if (createdDate < this.currentDate)
+                    {
+                        this.currentDate = createdDate;
+                    }
+                });
             }
         },
         components: {
             AddAccount,
-            Account
+            Account,
+            Datepicker
         },
         mounted() {
-            let accountsService = new AccountsService();
-            accountsService.get(getUser())
-            .then(response => {
-                response.forEach(account =>
-                {
-                    this.accounts.push(this.getDecryptedAccount(account, getUser().token));       
-                });
-            });
+            //mdc.autoInit()
+
+            this.fetchAccounts();
         },
         computed: {
-            filteredAccounts: function() {
-				return this.accounts.filter((account) => {
-				  return account.platform.toLowerCase().includes(this.search.toLowerCase());
-				});
-			  }
+            sortedAccounts: function () {
+                let accounts = this.$store.state.accounts;
+
+                return accounts.sort((account1, account2) => 
+                    sortByName(account1.displayPlatform, account2.displayPlatform)
+                );
+            },
+        
+            sortedTags: function () {
+                var tags = [];
+		
+                this.$store.state.accounts.forEach(function(account)
+                {
+                    let listTags = account.tags.split(',');
+
+                    listTags.forEach(function(tag)
+                    {
+                        tag = tag.trim();
+                        
+                        if (tags.indexOf(tag) == -1)
+                        {
+                            tags.push(tag);
+                        }
+                    });
+                });
+                
+                return tags.sort(sortByName);
+            },
+
+            currentTag: function () {
+                let tag = this.$route.params.tag;
+
+                if (!tag) {
+                    tag = 'All';
+                }
+
+                return tag;
+            }
+        },
+        watch:
+        {
+            currentDate: function (newValue)
+            {
+                this.currentDate = newValue;
+            }
         }
     }
 </script>
