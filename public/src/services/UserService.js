ca23a2c8fdb5ba5540994b8c603b2aa18f97e917
Ismaïl <nguyen.ismail@gmail.com>
2018-02-21 00:41:36 +0100

    Add session creation/deletion in userservice

--- public/src/services/UserService.js
+++ public/src/services/UserService.js
@@ -1,8 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
+
+var router = new VueRouter({
+    mode: 'history'
+});
+
 import { getUser, createSession, destroySession } from '../utils/auth'
+import { BASE_API_URL } from '../utils/api'
+import { getHeaders, getHeadersWithAuth } from '../utils/requestHeader'
 
 function UserService()
 {
-    const USERS_API_URL = '/api/users/';
+    const USERS_API_URL = BASE_API_URL + 'users/';
+
+    this.update = function (user)
+    {
+        user.last_update_date = new Date();
+
+        return fetch(USERS_API_URL + user._id,
+        {
+            method: 'PUT',
+            headers: getHeadersWithAuth(user.email, user.token),
+            body: JSON.stringify(user)
+        })
+        .then(response => createSession(user));
+    }
 
     this.login = function (email, password)
     {
@@ -12,22 +36,28 @@ function UserService()
             password: password
         };
 
-        return fetch(USERS_API_URL + 'login/', 
+        return fetch(USERS_API_URL + 'login/',
         {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+            headers: getHeaders(),
             body: JSON.stringify(credentials)
         })
         .then(handleLoginErrors)
         .then(response => response.json())
-        .then(user => createSession(user));
+        .then(user => 
+        {
+            createSession(user);
+
+            router.push('/');
+            location.reload();
+        });
     }
 
     this.logout = function ()
     {
         destroySession();
+
+        router.go('/');
     }
 
     function handleLoginErrors (response)
