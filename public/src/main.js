7ce2b3a0c90f8307b1ed7e5bffb318a5a0f3193e
Ismaïl <nguyen.ismail@gmail.com>
2018-02-21 00:41:02 +0100

    Register service worker on main

--- public/src/main.js
+++ public/src/main.js
@@ -4,9 +4,30 @@ import App from './components/App.vue'
 import router from './router'
 import store from './store'
 
+if ('serviceWorker' in navigator)
+{
+    if (navigator.serviceWorker.controller)
+    {
+        console.log('[PWA Builder] active service worker found, no need to register')
+    }
+    else
+    {
+        //Register the ServiceWorker
+        navigator.serviceWorker.register('service-worker.js', 
+        {
+            scope: './'
+        }).then(function(reg) {
+            console.log('Service worker has been registered for scope:' + reg.scope);
+        });
+    }
+}
+
 new Vue({
     el: '#app',
     router,
     store,
+    beforeCreate() {
+		this.$store.commit('initialiseStore');
+	},
     render: h => h(App)
 });
