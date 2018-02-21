44958439c9d8db2c2aae87a70213d05c865c8fd1
Ismaïl <nguyen.ismail@gmail.com>
2018-02-22 00:00:55 +0100

    Temporary remove localstoarge for accounts

--- public/src/store/index.js
+++ public/src/store/index.js
@@ -14,13 +14,13 @@ let mutations =
     initialiseStore (state)
     {
         // Check if the store exists
-        if(localStorage.getItem('store'))
-        {
-            // Replace the state object with the stored item
-            this.replaceState(
-                Object.assign(state, JSON.parse(localStorage.getItem('store')))
-            );
-        }
+        // if(localStorage.getItem('store'))
+        // {
+        //     // Replace the state object with the stored item
+        //     this.replaceState(
+        //         Object.assign(state, JSON.parse(localStorage.getItem('store')))
+        //     );
+        // }
     },
 
     updateAccounts (state, newValue)
@@ -54,7 +54,7 @@ const cachePlugin = store =>
     store.subscribe((mutation, state) =>
     {
         // Store the state object as a JSON string
-        localStorage.setItem('store', JSON.stringify(state));
+        //localStorage.setItem('store', JSON.stringify(state));
     })
 };
 
