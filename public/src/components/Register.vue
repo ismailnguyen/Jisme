80384e4ccaff8baa5580e93c6c4e8643dde613c1
Ismaïl <nguyen.ismail@gmail.com>
2018-05-24 00:48:23 +0200

    Add external libraries

--- public/src/components/Register.vue
+++ public/src/components/Register.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-center">
         <form class="form-signin">
-            <img class="mb-4" src="http://jisme.herokuapp.com/images/touch/favicon64.png" alt="" width="72" height="72">
+            <img class="mb-4" src="images/touch/favicon64.png" alt="" width="72" height="72">
 
             <h1 class="h3 mb-3 font-weight-normal">Sign Up</h1>
 
