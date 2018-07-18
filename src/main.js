35028c0d9dec5ea4309438e6ae865d052dc01997
Ismaïl <nguyen.ismail@gmail.com>
2018-07-19 00:30:39 +0200

    Move files

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import App from './components/App.vue'
 import router from './router'
 import store from './store'
 
-if ('serviceWorker' in navigator) {
+if (process.env.NODE_ENV === 'production') {
+  if ('serviceWorker' in navigator) {
     // Delay registration until after the page has loaded, to ensure that our
     // precaching requests don't degrade the first visit experience.
     // See https://developers.google.com/web/fundamentals/instant-and-offline/service-worker/registration
@@ -47,6 +48,7 @@ if ('serviceWorker' in navigator) {
       });
     });
   }
+}
 
 new Vue({
     el: '#app',
