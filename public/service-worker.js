4d21c998f27c1cdd73d4ea94e7604643a8a839aa
Ismaïl <nguyen.ismail@gmail.com>
2018-10-18 11:28:10 +0200

    Prevent service worker when cache request is 'only-if-cached' et mode not from same origin

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,61 +20,65 @@ var precacheFiles = [
 
 //Install stage sets up the cache-array to configure pre-cache content
 self.addEventListener('install', event => {
-  console.log('The service worker is being installed.');
-  event.waitUntil(precache().then(function() {
-    console.log('[ServiceWorker] Skip waiting on install');
-    return self.skipWaiting();
-  })
-  );
+      console.log('The service worker is being installed.');
+      
+      if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+      
+      event.waitUntil(precache().then(function() {
+            console.log('[ServiceWorker] Skip waiting on install');
+            return self.skipWaiting();
+      }));
 });
 
 //allow sw to control of current page
 self.addEventListener('activate', event => {
-  console.log('[ServiceWorker] Claiming clients for current page');
-  return self.clients.claim();
+      console.log('[ServiceWorker] Claiming clients for current page');
+      return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  console.log('The service worker is serving the asset.'+ event.request.url);
+      console.log('The service worker is serving the asset.'+ event.request.url);
+      
+      if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
 
-  if (event.request.url.includes('login') || event.request.url.includes('register'))
-  {
-    event.respondWith(fromServer(event.request).catch(fromCache(event.request)));
-  }
-  else
-  {
-    event.respondWith(fromCache(event.request).catch(fromServer(event.request)));
-  }
+      if (event.request.url.includes('login') || event.request.url.includes('register'))
+      {
+            event.respondWith(fromServer(event.request).catch(fromCache(event.request)));
+      }
+      else
+      {
+            event.respondWith(fromCache(event.request).catch(fromServer(event.request)));
+      }
 
-  event.waitUntil(update(event.request));
+      event.waitUntil(update(event.request));
 });
 
 function precache() {
-  return caches.open(CACHE).then(cache => {
-    return cache.addAll(precacheFiles);
-  });
+      return caches.open(CACHE).then(cache => {
+            return cache.addAll(precacheFiles);
+      });
 }
 
 function fromCache(request) {
-  //we pull files from the cache first thing so we can show them fast
-  return caches.open(CACHE).then(cache => {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
+      //we pull files from the cache first thing so we can show them fast
+      return caches.open(CACHE).then(cache => {
+            return cache.match(request).then(function (matching) {
+                  return matching || Promise.reject('no-match');
+            });
+      });
 }
 
 function update(request) {
-  //this is where we call the server to get the newest version of the 
-  //file to use the next time we show view
-  return caches.open(CACHE).then(cache => {
-    return fetch(request).then(response => {
-      return cache.put(request, response);
-    });
-  });
+      //this is where we call the server to get the newest version of the 
+      //file to use the next time we show view
+      return caches.open(CACHE).then(cache => {
+            return fetch(request).then(response => {
+                  return cache.put(request, response);
+            });
+      });
 }
 
 function fromServer(request){
-  //this is the fallback if it is not in the cahche to go to the server and get it
-  return fetch(request).then(response => { return response})
+      //this is the fallback if it is not in the cahche to go to the server and get it
+      return fetch(request).then(response => { return response})
 }
