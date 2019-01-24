efde89021815050257867517d3fd21f2c42c72f9
Ismaïl <nguyen.ismail@gmail.com>
2019-01-24 15:14:22 +0100

    Clone request & response on service worker due to non reusability

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@
 var CACHE = 'jisme-precache';
 var precacheFiles = [
       '/',
-  	  '/index.html',
+  	'/index.html',
       '/css/fontawesome.min.css',
       '/css/bootstrap.min.css',
       '/fonts/fontawesome-webfont.eot',
@@ -22,9 +22,19 @@ var precacheFiles = [
 self.addEventListener('install', event => {
       console.log('The service worker is being installed.');
       
-      if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+      // IMPORTANT: Clone the request. A request is a stream and
+      // can only be consumed once. Since we are consuming this
+      // once by cache and once by the browser for fetch, we need
+      // to clone the response.
+      var fetchRequest = event.request.clone();
       
-      event.waitUntil(precache().then(function() {
+      if (fetchRequest.cache === 'only-if-cached' && fetchRequest.mode !== 'same-origin') 
+      {
+            return;
+      }
+      
+      event.waitUntil(precache()
+                      .then(() => {
             console.log('[ServiceWorker] Skip waiting on install');
             return self.skipWaiting();
       }));
@@ -39,46 +49,64 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
       console.log('The service worker is serving the asset.'+ event.request.url);
       
-      if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
-
-      if (event.request.url.includes('login') || event.request.url.includes('register'))
+      // IMPORTANT: Clone the request. A request is a stream and
+      // can only be consumed once. Since we are consuming this
+      // once by cache and once by the browser for fetch, we need
+      // to clone the response.
+      var fetchRequest = event.request.clone();
+      
+      if (fetchRequest.cache === 'only-if-cached' && fetchRequest.mode !== 'same-origin')
       {
-            event.respondWith(fromServer(event.request).catch(fromCache(event.request)));
+            return;
+      }
+
+      let notCachedPages = ['login', 'register'];
+      if (notCachedPages.some(page => fetchRequest.url.indexOf(page) >= 0))
+      {
+            event
+                  .respondWith(fromServer(fetchRequest)
+                  .catch(fromCache(fetchRequest)));
       }
       else
       {
-            event.respondWith(fromCache(event.request).catch(fromServer(event.request)));
+            event
+                  .respondWith(fromCache(fetchRequest)
+                  .catch(fromServer(fetchRequest)));
       }
 
-      event.waitUntil(update(event.request));
+      event.waitUntil(update(fetchRequest));
 });
 
 function precache() {
-      return caches.open(CACHE).then(cache => {
-            return cache.addAll(precacheFiles);
-      });
+      return caches.open(CACHE)
+                  .then(cache => cache.addAll(precacheFiles));
 }
 
 function fromCache(request) {
       //we pull files from the cache first thing so we can show them fast
-      return caches.open(CACHE).then(cache => {
-            return cache.match(request).then(function (matching) {
-                  return matching || Promise.reject('no-match');
-            });
-      });
+      return caches.open(CACHE)
+                  .then(cache => cache.match(request)
+                                    .then(matching => matching || Promise.reject('no-match'))
+                       );
 }
 
 function update(request) {
       //this is where we call the server to get the newest version of the 
       //file to use the next time we show view
-      return caches.open(CACHE).then(cache => {
-            return fetch(request).then(response => {
-                  return cache.put(request, response);
-            });
-      });
+      return caches.open(CACHE)
+                        .then(cache => fetch(request)
+                              .then(response => {
+                                    // IMPORTANT: Clone the response. A response is a stream
+                                    // and because we want the browser to consume the response
+                                    // as well as the cache consuming the response, we need
+                                    // to clone it so we have two streams.
+                                    var responseToCache = response.clone();
+
+                                    cache.put(request, responseToCache)
+      }));
 }
 
 function fromServer(request){
       //this is the fallback if it is not in the cahche to go to the server and get it
-      return fetch(request).then(response => { return response})
+      return fetch(request).then(response => response.clone())
 }
