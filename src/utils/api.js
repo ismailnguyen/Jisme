2bd9b89d2c9a8c27f6350bdd6a8be9b35bcc8735
Ismaïl NGUYEN <ismail.nguyen@loccitane.com>
2022-09-18 22:57:52 +0200

    change api url to the proxy one

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,3 @@
 //export const BASE_API_URL = 'http://localhost:8888/' // Dev mode
-export const BASE_API_URL = 'https://cors-proxy-eu.herokuapp.com/https://api.jisme.app/'; // Bypass CORS for local developments
-//export const BASE_API_URL = 'https://api.jisme.app/';
+//export const BASE_API_URL = 'https://cors-proxy-eu.herokuapp.com/https://api.jisme.app/'; // Bypass CORS for local developments
+export const BASE_API_URL = 'https://jisme.app/api/';
