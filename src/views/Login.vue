a6fb7d8fbc8950bd6fcbef4fb6426ccf9889ba01
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2023-01-27 17:08:29 +0100

    two step login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,25 +4,26 @@
             <img class="mb-4" src="images/touch/favicon64.png" alt="" width="72" height="72" v-show="!isLoading">
             <Loader v-show="isLoading" />
 
-            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+            <h1 class="h3 mb-3 font-weight-normal">Sign in</h1>
 
             <p class="text-danger">{{error.message}}</p>
 
-            <label for="inputEmail" class="sr-only">Email address</label>
-            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" aria-describedby="emailHelp" v-model="email" @keyup.enter="handleLogin()" required>
+            <label v-if="!isEmailFilled" for="inputEmail" class="sr-only">Email address, phone number</label>
+            <input v-if="!isEmailFilled" type="email" id="inputEmail" class="form-control" placeholder="Email address, phone number" aria-describedby="emailHelp" v-model="email" @keyup.enter="submitEmail()" required>
 
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input type="password" id="inputPassword" class="form-control" placeholder="Password" v-model="password" @keyup.enter="handleLogin()" required>
+            <label v-if="isEmailFilled" for="inputPassword" class="sr-only">Password</label>
+            <input v-if="isEmailFilled" type="password" id="inputPassword" class="form-control" placeholder="Password" v-model="password" @keyup.enter="handleLogin()" required>
 
-            <div class="checkbox mb-3">
+            <div v-if="!isEmailFilled" class="checkbox mb-3">
                 <label>
                     <input type="checkbox" value="remember-me" v-model="remember"> Remember me
                 </label>
             </div>
 
-            <span class="btn btn-lg btn-outline-primary btn-block" @click="handleLogin">Sign in</span>
+            <span class="btn btn-lg btn-outline-primary btn-block" @click="submitEmail" v-if="!isEmailFilled">Next</span>
+            <span class="btn btn-lg btn-outline-primary btn-block" @click="handleLogin" v-if="isEmailFilled">Sign in</span>
 
-            <p class="mt-5 mb-3 text-muted"><router-link to="/register">Don't have account ? Sign up</router-link></p>
+            <p class="mt-5 mb-3 text-muted">Don't have account? <router-link to="/register">Sign up</router-link></p>
         </form>
     </div>
 </template>
@@ -39,6 +40,7 @@
                 error: {
                     message: ''
                 },
+                isEmailFilled: false,
                 remember: false,
                 isLoading: false
             }
@@ -47,6 +49,10 @@
             Loader
         },
         methods: {
+            submitEmail: function () {
+                this.isEmailFilled = true;
+            },
+
             handleLogin: function ()
             {
                 this.isLoading = true;
@@ -131,25 +137,19 @@
         z-index: 2;
     }
 
-    .form-signin input[type="email"] {
-        margin-bottom: -1px;
-        border-bottom-right-radius: 0;
-        border-bottom-left-radius: 0;
-    }
-
+    .form-signin input[type="email"],
     .form-signin input[type="password"] {
         margin-bottom: 10px;
-        border-top-left-radius: 0;
-        border-top-right-radius: 0;
     }
 
     .form-signin .btn-outline-primary {
         color: #162056;
-        border-color: #162056;
+        border-color: #162056 !important;
     }
 
-    .form-signin .btn-outline-primary:hover {
-        color: #f8f9fa;
-        background-color: #162056;
+    .form-signin .btn-outline-primary:hover,
+    .form-signin .btn-outline-primary:active {
+        color: #f8f9fa !important;
+        background-color: #162056 !important;
     }
 </style>
