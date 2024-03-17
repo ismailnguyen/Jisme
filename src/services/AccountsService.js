e75e029112ad86e7cac5678bd336716366ebf61e
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-03-17 19:41:31 +0800

    move out account ui logics to account store

--- src/services/AccountsService.js
+++ src/services/AccountsService.js
@@ -1,127 +1,178 @@
-import { getEncryptedAccount, getDecryptedAccount, parseAccount } from '../utils/account'
+import { 
+    LOCAL_STORAGE_DB_NAME,
+    LOCAL_STORAGE_ACCOUNTS_KEY,
+    LOCAL_STORAGE_RECENT_ACCOUNTS_KEY
+ } from '../utils/storage'
+import localforage from 'localforage'
+import {
+    getEncryptedAccount,
+    getDecryptedAccount,
+    parseAccount,
+    getEncryptionKey
+} from '../utils/account'
 import { BASE_API_URL } from '../utils/api'
 import { getHeadersWithAuth } from '../utils/requestHeader'
 import { Exception, SessionExpiredException } from '../utils/errors'
 
-function AccountsService (user, store)
+localforage.config({
+    name: LOCAL_STORAGE_DB_NAME
+});
+
+function AccountsService (user)
 {
     this.user = user;
-    this.store = store;
     this.headers = getHeadersWithAuth(user.token);
 
     const ACCOUNTS_API_URL = BASE_API_URL + 'accounts/';
 
-    this.get = function(account)
+    this.get = async function(account)
     {
-        return fetch(ACCOUNTS_API_URL + account._id,
-        {
-            method: 'GET',
-            headers: this.headers
-        })
-        .then(handleErrors)
-        .then(encryptedAccount => parseAccount(getDecryptedAccount(encryptedAccount, this.user.uuid)))
-        .then(account => 
-        {	
-            console.log(account)
-        })
-        .catch(throwError);
+        try {
+            const response = await fetch(AACCOUNTS_API_URL + account._id,
+            {
+                method: 'GET',
+                headers: this.headers
+            })
+
+            const encryptedAccount = await response.json();
+
+            return parseAccount(getDecryptedAccount(encryptedAccount, getEncryptionKey(this.user)))
+        }
+        catch(error) {
+            throwError (error);
+        }
     }
 
-    this.getRecents = function () {
-        return fetch(ACCOUNTS_API_URL + 'recents/',
-        {
-            method: 'GET',
-            headers: this.headers
-        })
-        .then(handleErrors)
-        .then(accounts => accounts.map(account => parseAccount(getDecryptedAccount(account, this.user.uuid))))
-        .then(accounts => 
-        {	
-            this.store.commit('updateAccounts', accounts);
+    this.getRecentsCached = async function() {
+        const cachedAccounts = await localforage.getItem(LOCAL_STORAGE_RECENT_ACCOUNTS_KEY);
 
-            return accounts;
-        })
-        .catch(throwError);
-    },
-
-    this.getAll = function()
-    {
-        return fetch(ACCOUNTS_API_URL,
-        {
-            method: 'GET',
-            headers: this.headers
-        })
-        .then(handleErrors)
-        .then(accounts => accounts.map(account => parseAccount(getDecryptedAccount(account, this.user.uuid))))
-        .then(accounts => 
-        {	
-            this.store.commit('updateAccounts', accounts);
-
-            return accounts;
-        })
-        .catch(throwError);
-    }
-
-    this.add = function(accountToAdd)
-    {
-        let encryptedAccount = getEncryptedAccount(accountToAdd, this.user.uuid);
-
-        return fetch(ACCOUNTS_API_URL,
-        {
-            method: 'POST',
-            headers: this.headers,
-            body: JSON.stringify(encryptedAccount)
-        })
-        .then(handleErrors)
-        //.then(response => response.clone().json())
-        .then(addedAccount => parseAccount(getDecryptedAccount(addedAccount, this.user.uuid)))
-        .then(addedAccount => this.store.commit('addAccount', addedAccount))
-        .catch(throwError);
-    }
-
-    this.save = function(accountToSave)
-    {
-        let encryptedAccount = getEncryptedAccount(accountToSave, this.user.uuid);
-        return fetch(ACCOUNTS_API_URL + accountToSave._id,
-        {
-            method: 'PUT',
-            headers: this.headers,
-            body: JSON.stringify(encryptedAccount)
-        })
-        .then(handleErrors)
-        .then(response => this.store.commit('updateAccount', accountToSave))
-        .catch(throwError);
-    }
-
-    this.remove = function(accountToRemove)
-    {
-        return fetch(ACCOUNTS_API_URL + accountToRemove._id,
-        {
-            method: 'DELETE',
-            headers: this.headers
-        })
-        .then(handleErrors)
-        .then(response => this.store.commit('removeAccount', accountToRemove))
-        .catch(throwError);
-    }
-
-    function handleErrors (response)
-    {
-        if (!response.ok)
-        {
-            throw response;
+        if (cachedAccounts) {
+            return cachedAccounts.map(account => parseAccount(getDecryptedAccount(account, getEncryptionKey(this.user))));
         }
 
-        return response.json();
+        return [];
     }
 
-    function throwError (response) {
-        if (response.status === 401) {
+    this.getRecents = async function () {
+        try {
+            const response = await fetch(ACCOUNTS_API_URL + 'recents/',
+            {
+                method: 'GET',
+                headers: this.headers
+            })
+
+            const recentEncryptedAccounts = await response.json();
+            return recentEncryptedAccounts.map(account => parseAccount(getDecryptedAccount(account, getEncryptionKey(this.user))))
+        }
+        catch(error) {
+            throwError (error);
+        }
+    },
+
+    this.getAllCached = async function() {
+        const cachedAccounts = await localforage.getItem(LOCAL_STORAGE_ACCOUNTS_KEY);
+
+        if (cachedAccounts) {
+            return cachedAccounts.map(account => parseAccount(getDecryptedAccount(account, getEncryptionKey(this.user))));
+        }
+
+        return [];
+    }
+
+    this.getAll = async function()
+    {
+        try {
+            const response = await fetch(ACCOUNTS_API_URL,
+            {
+                method: 'GET',
+                headers: this.headers
+            })
+
+            const encryptedAccounts = await response.json();
+
+            return encryptedAccounts.map(account => parseAccount(getDecryptedAccount(account, getEncryptionKey(this.user))))
+        }
+        catch(error) {
+            throwError (error);
+        }
+    }
+
+    this.add = async function(accountToAdd)
+    {
+        let encryptedAccount = getEncryptedAccount(accountToAdd, getEncryptionKey(this.user));
+
+        try {
+            const response = await fetch(ACCOUNTS_API_URL,
+            {
+                method: 'POST',
+                headers: this.headers,
+                body: JSON.stringify(encryptedAccount)
+            })
+
+            const addedAccount = await response.json();
+
+            return parseAccount(getDecryptedAccount(addedAccount, getEncryptionKey(this.user)))
+        }
+        catch(error) {
+            throwError (error);
+        }
+    }
+
+    this.save = async function(accountToSave)
+    {
+        let encryptedAccount = getEncryptedAccount(accountToSave, getEncryptionKey(this.user));
+
+        try {
+            const response = await fetch(ACCOUNTS_API_URL + accountToSave._id,
+            {
+                method: 'PUT',
+                headers: this.headers,
+                body: JSON.stringify(encryptedAccount)
+            })
+
+            const savedAccount = await response.json();
+
+            return parseAccount(getDecryptedAccount(savedAccount, getEncryptionKey(this.user)))
+        }
+        catch(error) {
+            throwError (error);
+        }
+    }
+
+    this.remove = async function(accountToRemove)
+    {
+        try {
+            await fetch(ACCOUNTS_API_URL + accountToRemove._id,
+            {
+                method: 'DELETE',
+                headers: this.headers
+            })
+
+            return accountToRemove
+        }
+        catch(error) {
+            throwError (error);
+        }
+    }
+    
+    this.updateLocalRecentAccounts = async function(accounts) {
+        await localforage.setItem(LOCAL_STORAGE_RECENT_ACCOUNTS_KEY,
+            accounts.map(account => parseAccount(getEncryptedAccount(account, getEncryptionKey(this.user))))
+        );
+    }
+
+    this.updateLocalAccounts = async function(accounts) {
+        await localforage.setItem(LOCAL_STORAGE_ACCOUNTS_KEY,
+            accounts.map(account => parseAccount(getEncryptedAccount(account, getEncryptionKey(this.user))))
+        );
+    }
+
+    function throwError (error) {
+        if (error.status === 401) {
             throw new SessionExpiredException();
         }
 
-        response.text().then(console.log);
-        throw new Exception('Error', 'Please retry', response.status);
+        throw new Exception('Error', 'Please retry', error.status);
     }
 };
 
