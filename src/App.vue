2405525e6852bb552ef7fdc76a298e4435c5aabf
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-03-17 19:38:15 +0800

    move account logics from app to account list view

--- src/App.vue
+++ src/App.vue
@@ -1,193 +1,52 @@
 <template>
-    <div id="app" :class="isMenuToggled ? 'menuDisplayed' : ''">
-        <Menu
-            v-on:toggleAddAccountModal="onAddAccountModalToggled"
-            v-on:toggleMenu="onToggleMenu"
-            :user="user"
-            :searchQuery="searchQuery"
-            v-if="displayMenus" />
- 
-        <AddAccountModal 
-            :user="user" 
-            v-on:showAlert="onShowAlert"
-            v-on:toggleAddAccountModal="onAddAccountModalToggled"
-            v-if="displayMenus && showAddAccountModal" />
+    <div>
+        <RouterView />
 
-        <router-view
-            :user="user"
-            v-on:showAlert="onShowAlert"
-            :addAccountModalToggled="showAddAccountModal"
-            v-on:searchQueryUpdated="onSearchQueryUpdated" />
-
-        <a id="menu-toggle" class="floating-button" :class="isMenuToggled ? 'floating-button--close' : 'floating-button--menu'" @click="onToggleMenu" v-show="displayMenus">
-            <i class="fa fa-solid" :class="isMenuToggled ? 'fa-close' : 'fa-bars'"></i>
-        </a>
-
-        <a id="menu-toggle" class="floating-button floating-button--add d-none d-lg-block d-xl-block" @click="onAddAccountModalToggled" v-if="!isMenuToggled && displayMenus">
-            <i class="fa fa-plus"></i>
-        </a>
-
-        <AlertBox :alertDetails="alertDetails" v-if="showAlert" v-on:closeAlert="onToggleAlert" />
+        <AlertBox v-if="hasAlert" />
     </div>
 </template>
 
 <script>
-    import { getUser } from './utils/auth'
-    import UserService from './services/UserService'
-    import AccountsService from './services/AccountsService'
-    import Menu from './components/Menu.vue'
-    import Alert from './models/Alert'
+    import { useAlertStore } from '@/store'
+    import { storeToRefs } from 'pinia'
     import AlertBox from './components/AlertBox.vue'
-    import AddAccountModal from './components/AddAccount.vue'
-    import { SessionExpiredException } from './utils/errors'
-    import 'regenerator-runtime/runtime';
 
     export default {
         data () {
             return {
-                user: getUser(),
-                alertDetails: {},
-                showAddAccountModal: false,
-                showAlert: false,
-                isMenuToggled: false,
-                searchQuery: '',
+            }
+        },
+        setup() {
+            const alertStore = useAlertStore()
+            const { hasAlert } = storeToRefs(alertStore)
+            const { openAlert, clearAlert } = alertStore
+
+            return {
+                alertStore,
+                hasAlert,
+                openAlert,
+                clearAlert
             }
         },
         components: {
-            Menu,
-            AlertBox,
-            AddAccountModal,
+            AlertBox
         },
-        mounted() {
-            if (this.user) {
-                // If there is no account to display at loading, first retrieve the 10 most recents
-                // then load asynchronously the rest
-                if (!this.$store.state.accounts || !this.$store.state.accounts) {
-                    this.fetchRecentAccounts()
-                    .then(() => {
-                        this.onShowAlert(
-                            new Alert('Refreshing', 'Please wait...', 'info')
-                        )
-
-                        this.fetchAccounts()
-                        .then(() => {
-                            this.onShowAlert(
-                                new Alert('Refreshing', 'Done.', 'success')
-                            )
-                        });
-                    });
-                } else {
-                    this.onShowAlert(
-                        new Alert('Refreshing', 'Please wait...', 'info')
-                    )
-                    this.fetchAccounts()
-                    .then(() => {
-                        this.onShowAlert(
-                            new Alert('Refreshing', 'Done.', 'success')
-                        )
-                    });
+        created() {
+            this.alertStore.$subscribe((mutation, state) => {
+                if (this.hasAlert) {
+                    this.showAlert();
                 }
-            }
-        },
-        computed: {
-            accounts: function () {
-                return this.$store.state.accounts;
-            },
-
-            displayMenus: function () {
-                return this.user
-                    && this.$route
-                    && this.$route.matched
-                    && this.$route.matched.length
-                    && this.$route.matched[0].props
-                    && this.$route.matched[0].props.default
-                    && this.$route.matched[0].props.default.menubar;
-            }
+            })
         },
         methods: {
-            fetchRecentAccounts: async function () {
-                const accountsService = new AccountsService(this.user, this.$store);
-
-                return accountsService.getRecents()
-                .catch(error =>
-                {
-                    this.onShowAlert(
-                        new Alert(error.name, error.message, 'danger')
-                    )
-
-                    if (error instanceof SessionExpiredException) {
-                        let userService = new UserService();
-                        userService.logout(() => {
-                            this.$router.go('/');
-                        });
-                    }
-                });
-            },
-    
-            fetchAccounts: async function () {
-                const accountsService = new AccountsService(this.user, this.$store);
-
-                return accountsService.getAll()
-                .catch(error =>
-                {
-                    this.onShowAlert(
-                        new Alert(error.name, error.message, 'danger')
-                    )
-
-                    if (error instanceof SessionExpiredException) {
-                        let userService = new UserService();
-                        userService.logout(() => {
-                            this.$router.go('/');
-                        });
-                    }
-                });
-            },
-
-            onAddAccountModalToggled: function () {
-                this.showAddAccountModal = !this.showAddAccountModal;
-                this.addAccountModalToggled = !this.addAccountModalToggled;
-            },
-
-            onShowAlert: function (alertDetails)
-            {
-                this.alertDetails = alertDetails;
-
-                if (!this.showAlert) {
-                    this.onToggleAlert();
-
-                    setTimeout(() => {
-                        this.onToggleAlert();
-                    }
-                    , 5000);
+            showAlert: function () {
+                // After few seconds, clear the alert
+                setTimeout(() => {
+                    this.clearAlert();
                 }
+                , 5000);
             },
-
-            onToggleAlert: function () {
-                this.showAlert = !this.showAlert;
-            },
-
-            onToggleMenu: function () {
-                this.isMenuToggled = !this.isMenuToggled;
-            },
-
-            triggerModalOpened: function (isModalOpened) {
-				if (isModalOpened) {
-					document.body.classList.add('modal-open');
-				}
-				else {
-					document.body.classList.remove('modal-open');
-				}
-			},
-
-            onSearchQueryUpdated: function (newValue) {
-                this.searchQuery = newValue;
-            }
-        },
-        watch: {
-			showAddAccountModal: function () {
-				this.triggerModalOpened(this.showAddAccountModal);
-			},
-		}
+        }
     }
 </script>
 
@@ -217,155 +76,7 @@
 		}
 	}
 
-    a {
-      color: #162056;
-    }
-
-    .main-container {
-        padding-top: 80px;
-    }
-
-    @media (min-width: 767.98px) { 
-        .main-container:last-child {
-            padding-top: 100px;
-        }
-    }
-
-    @media (max-width: 767.98px) { 
-        .main-container {
-            padding-top: 30px;
-        }
-
-        .main-container:last-child {
-            padding-bottom: 100px;
-        }
-    }
-	  
-    header {
-        width: 100%;
-        position: fixed;
-        color: #162056;
-        padding: 20px 20px 20px 40px;
-        z-index: 99;
-    }
-
-    .form-control {
-        color: #f8f9fa;
-        border: none;
-        border-radius: 15px;
-        background-color: #ced4da70;
-    }
-
-    .form-control::placeholder {
-        color: #f8f9fa;
-        opacity: 1;
-    }
-
     .btn {
         border-radius: 15px;
-    }
-
-    /* Main Content */
-    #page-content-wrapper {
-        width: 100%;	
-        position: absolute;
-        transition: all .5s;
-    }
-
-    #menu-toggle {
-        transition: all .3s;
-        font-size: 2em;
-    }
-
-    /* Change the width of the sidebar to display it*/
-    #app.menuDisplayed #sidebar-wrapper {
-        position: fixed;
-        width: 250px;
-    }
-
-    @media (min-width: 767.98px) { 
-        #app.menuDisplayed #menu-toggle {
-            top: 20px;
-            right: 265px;
-        }
-    }   
-
-    @media (max-width: 767.98px) { 
-        #app.menuDisplayed #sidebar-wrapper {
-            width: 100%;
-            height: 100%;
-        }
-    }
-
-    @media (min-width: 767.98px) { 
-        #app.menuDisplayed #page-content-wrapper {
-            padding-right: 250px;
-        }
-    }
-
-    .badge-primary {
-        color: #fff;
-        background: linear-gradient(81.62deg,#2870ea 8.72%,#1b4aef 85.01%);
-    }
-
-    .badge-primary:hover {
-        color: #162056db;
-    }
-
-    .floating-button.floating-button--menu {
-        top: 20px;
-        right: 15px;
-        color: #fff;
-        background: linear-gradient(81.62deg,#2870ea 8.72%,#1b4aef 85.01%);
-    }
-
-    .floating-button.floating-button--close {
-        top: 20px;
-        right: 15px;
-        background: #fff;
-    }
-
-    .floating-button.floating-button--close i {
-        color: #162056;
-    }
-
-    .floating-button.floating-button--add {
-        top: 20px;
-        right: 80px;
-        color: #fff;
-        background-color: #dc3545;
-    }
-
-    .floating-button {
-        position: fixed;
-        cursor: pointer;
-        width: 48px;
-        height: 48px;
-        border-radius: 15px;
-        text-align: center;
-        background-color: #fff;
-        box-shadow: 9px 9px 16px rgb(163,177,198,0.6), -9px -9px 16px  rgba(255,255,255, 0.5);
-        z-index: 9999;
-    }
-
-    .floating-button.floating-button--menu:hover,
-    .floating-button.floating-button--add:hover {
-        box-shadow: 2px 4px 16px rgb(0 0 0 / 16%);
-        transform: scale3d(1.01,1.01,1.01);
-    }
-
-    .floating-button i {
-        color: #eee;
-    }
-	
-	@media (prefers-color-scheme: dark) {
-		.floating-button {
-			box-shadow: none;
-		}
-	}
-
-    .badge-pill {
-        margin-right: 5px;
-        cursor: pointer;
     }
 </style>
---
+++ src/components/AccountList.vue
@@ -0,0 +1,296 @@
+<template>
+    <div id="page-content-wrapper" class="container-fluid">
+        <header class="row header-search justify-content-center" v-if="!loading">
+            <div class="col-xs-12 col-lg-6">
+                <input class="form-control searchBar" :value="searchQuery" @input="updateSearchQuery" type="search" placeholder="Search" autofocus>
+            </div>
+        </header>
+
+        <div class="main-container container-fluid" v-if="isSearching">
+            <span
+                class="badge badge-pill badge-primary"
+                v-for="(tag, tagIndex) in selectedTags"
+                v-bind:key="tagIndex"
+                @click="removeTag(tag)">
+                {{ tag }}
+                <i class="fa fa-close"></i>
+            </span>
+            <br><br>
+            <div class="row" v-if="!loading">
+                <AccountItem 
+                    v-for="(account, accountIndex) in truncate(accountsFilteredByQuery)"
+                    v-bind:key="accountIndex"
+                    :account="account" />
+            </div>
+        </div>
+        <div class="main-container container-fluid" v-else>
+            <span class="category-title">Recently viewed</span>
+            <br><br>
+
+            <StackedAccountList :accounts="recentAccounts" />
+        </div>
+
+        <div class="row loadMore justify-content-center" v-if="accountsFilteredByQuery && accountsFilteredByQuery.length > truncate(accountsFilteredByQuery).length && !loading && searchQuery">
+            <div class="col-xs-12 col-lg-6">
+                <button id="loadMore" @click="loadMore()" type="button" class="btn btn-lg btn-light btn-block load-more-button">
+                    More
+                </button>
+            </div>
+        </div>
+
+        <Loader v-show="loading" />
+    </div>
+</template>
+
+<script>
+    import { onBeforeMount } from 'vue'
+    import { storeToRefs } from 'pinia'
+    import {
+        useAccountsStore,
+        useAlertStore,
+        useUserStore
+     } from '@/store'
+    import Account from '../models/Account'
+    import Alert from '../models/Alert'
+    import { SessionExpiredException } from '../utils/errors'
+    import EditAccountModal from '../components/EditAccount.vue'
+    import AccountItem from '../components/AccountItem.vue'
+    import StackedAccountList from '../components/StackedAccountList.vue'
+    import Loader from '../components/Loader.vue'
+    
+    export default {
+        data() {
+            return {
+                searchQuery: this.$route.query.search || '', // Default search query is looked up from query string
+                loading: true,
+                pagination_offset: 0,
+                editAccount: new Account(),
+            }
+        },
+        components: {
+            EditAccountModal,
+            AccountItem,
+            StackedAccountList,
+            Loader,
+        },
+        setup() {
+            const accountsStore = useAccountsStore()
+            const { signOut } = useUserStore()
+            const { openAlert } = useAlertStore()
+
+            const { recentAccounts, accounts } = storeToRefs(accountsStore)
+            const { loadCache, fetchRecentAccounts, fetchAccounts, getAccountsFilteredByQuery } = accountsStore
+
+            onBeforeMount(async () => {
+                await loadCache()
+            })
+
+            return {
+                openAlert,
+                fetchRecentAccounts,
+                fetchAccounts,
+                accountsStore,
+                getAccountsFilteredByQuery,
+                accounts,
+                recentAccounts,
+                signOut
+            }
+        },
+        created() {
+            this.accountsStore.$subscribe((mutation, state) => {
+                this.loading = false;
+            })
+        },
+        async mounted() {
+            await this.fetchLatestAccounts();
+
+            this.initPagination();
+            this.loadMoreOnScrollToBottom();
+        },
+        methods: {
+            fetchLatestAccounts: async function () {
+                try {
+                    await this.fetchRecentAccounts();
+                    await this.fetchAccounts();
+                } catch (error) {
+                    if (error instanceof SessionExpiredException) {
+                        this.openAlert(new Alert('Session expired', error.message, 'danger'));
+
+                        this.signOut();
+                        this.$router.go('/');
+                    }
+                    else {
+                        this.openAlert(new Alert('Error while loading accounts', error.message, 'danger'));
+                    }
+                }
+            },
+
+            removeTag: function (tag) {
+                let newTags = this.$route.query.tags.split(',').map(x => x.trim());
+                newTags.splice(newTags.indexOf(tag), 1);
+                
+                this.$router.push({name: 'AccountList', query: { tags: newTags.join(',') }});
+            },
+
+            updateSearchQuery: function (event)
+            {
+                this.searchQuery = event.target.value;
+            },
+
+            loadMore: function ()
+            {
+                this.pagination_offset += 1;
+            },
+
+            initPagination: function ()
+            {
+                this.pagination_offset = 20;
+            },
+
+            loadMoreOnScrollToBottom: function () {
+                window.onscroll = () => {
+                    let bottomOfWindow = (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+
+                    if (bottomOfWindow) {
+                        this.loadMore();
+                    }
+                };
+            },
+
+            // Function for testing purpose
+            getDuplicatesOnly: function (initalArray) {
+                var sorted_arr = initalArray;
+                var duplicateAccounts = [];
+
+                for (var i = 0; i < sorted_arr.length - 1; i++) {
+                    if (sorted_arr[i + 1].platform == sorted_arr[i].platform && sorted_arr[i + 1].login == sorted_arr[i].login) {
+                        duplicateAccounts.push(sorted_arr[i]);
+                    }
+                }
+
+                console.log('duplicateAccounts', duplicateAccounts.map(account => account.platform));
+
+                return duplicateAccounts;
+            },
+			
+            truncate: function (elements)
+            {
+                return elements//.slice(0, this.pagination_offset);
+            }
+        },
+        computed: {
+            accountsFilteredByQuery: function () {
+                return []
+                return this.getAccountsFilteredByQuery(this.searchQuery, this.$route.query.tags, true)
+            },
+
+            selectedTags: function () {
+                return this.$route.query.tags ? this.$route.query.tags.split(',').map(x => x.trim()) : [];
+            },
+
+            isSearching: function () {
+                return this.searchQuery || this.$route.query.tags;
+            }
+        }
+    }
+</script>
+
+<style>
+    /* Reduce search bar size when right sidebar is opened */
+    .accountOpened .header-search,
+    .settingsOpened .header-search {
+        padding-right: 50%;
+    }
+
+    /* Reduce search bar size when left sidebar menu is opened */
+    .menuDisplayed .header-search {
+        padding-left: 250px;
+    }
+
+    .menuDisplayed.accountOpened .header-search,
+    .menuDisplayed.settingsOpened .header-search{
+        padding-right: 70%;
+        padding-left: 10%;
+    }
+
+    .header-search {
+        margin: auto;
+    }
+    
+    @media (max-width: 767.98px) { 
+        .header-search {
+            position: fixed;
+            bottom: 10px;
+            left: 0;
+            right: 0;
+        }
+    }
+    
+    .searchBar {
+        border-radius: 15px;
+        color: #818182;
+        line-height: 2.5;
+        box-shadow: 20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff;
+        backdrop-filter: blur(4px);
+    }
+	
+	@media (prefers-color-scheme: dark) {
+		.searchBar {
+			box-shadow: none;
+		}
+	}
+
+    .searchBar::placeholder {
+        color: #818182;
+    }
+	
+	@media (prefers-color-scheme: dark) {	
+		.searchBar::placeholder {
+			color: #e4e6eb;
+		}
+	}
+
+    .searchBar:hover,
+    .searchBar:active,
+    .searchBar:focus {
+        border: none !important;
+        background-color: #ced4da70;
+		box-shadow: 2px 4px 16px rgb(0 0 0 / 16%);
+        transform: scale3d(1.01,1.01,1.01);
+	}
+	
+	@media (prefers-color-scheme: dark) {
+		.searchBar:hover,
+		.searchBar:active,
+		.searchBar:focus {
+			color: #fff;
+		}
+	}
+
+    .load-more-button {
+		width: 35%;
+        border-radius: 15px;
+		border: none;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 100px;
+        margin-bottom: 20px;
+        text-align: center;
+        background-color: #f8f9fa;
+        color: #162056;
+        box-shadow: 9px 9px 16px rgb(163,177,198,0.6), -9px -9px 16px  rgba(255,255,255, 0.5);
+    }
+
+    .load-more-button:hover {
+		box-shadow: inset -4px -4px 10px rgba(255,255,255,0.5), inset 4px 4px 10px rgba(0,0,0,0.1);
+	}
+	
+	@media (prefers-color-scheme: dark) {
+		.load-more-button,
+		.load-more-button:hover {
+			color: #e4e6eb;
+			background: #4b4c4f;
+			box-shadow: none;
+		}
+	}
+</style>
