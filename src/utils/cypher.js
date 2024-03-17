cfabea6b89835548332ebc23ed444f7e421034c0
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-03-17 19:45:23 +0800

    init new encryption method

--- src/utils/cypher.js
+++ src/utils/cypher.js
@@ -9,3 +9,9 @@ export function decrypt(encrypted, token)
 	let masterpass = sha256(token)
 	return sjcl.decrypt(masterpass, encrypted)
 }
+
+export function encryptionKey(publicKey)
+{
+	return `${publicKey}`
+	//return `${publicKey}${import.meta.env.VITE_PRIVATE_ENCRYPTION_KEY}`
+}
