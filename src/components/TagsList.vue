da009313996da4d76e4618b3572aa234b0aba88a
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-04-09 02:23:20 +0800

    update tag list design (wip)

--- src/components/TagsList.vue
+++ src/components/TagsList.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="tags-sidebar-wrapper">
-        <div class="tags-sidebar">
-            <div class="sidebar-header">
+        <div class="right-sidebar">
+             <div class="sidebar-header">
                 <div class="row">
                     <div class="form-group col-xs-9 col-sm-9 col-9 col-md-9 col-lg-9">
                         <h2 class="sidebar-title">Tags</h2>
@@ -16,35 +16,44 @@
             </div>
 
             <div class="sidebar-body">
-                <ul class="list-group">
-                    <li class="list-group-item">
-                        <div class="input-group">
+                <div class="row">
+                    <div class="mb-3 col-xs-12 col-md-12 col-lg-12">
+                        <label class="form-label" for="inputAvatar"><i class="fa fa-tag" aria-hidden="true"></i> New tag</label>
+                            <div class="input-group">
                             <input class="form-control" type="text" placeholder="Add a new tag" />
-                            <div class="input-group-append">
-                                <button class="btn btn-outline-secondary" type="button">Add</button>
-                            </div>
+                            <button class="btn btn-outline-secondary" type="button">Add</button>
                         </div>
-                    </li>
-                    <li
-                        class="list-group-item"
-                        v-for="(tag, index) in getUniqueTags()"
-                        v-bind:key="index"
-                        @click="selectTag(tag)">
-                        {{  tag.name || 'None' }} ({{ tag.count }})
+                    </div>
 
-                        <span class="pull-right">
-                        <span class="btn btn-xs btn-default" @click="selectTag(tag)">
-                            <i class="fa" :class="isCurrentTag(tag) ? 'fa-check-square-o' : 'fa-square-o'"></i>
-                        </span>
-                    </span>
-                    </li>
-                </ul>
+                    <div class="mb-3 col-xs-12 col-md-12 col-lg-12">
+                        <div class="mb-3 col-xs-12 col-md-12 col-lg-12">
+                            <label class="form-label" for="inputAvatar"><i class="fa fa-tags" aria-hidden="true"></i> Existing tags</label>
+                        </div>
+                        <ul class="list-group">
+                            <li
+                                class="list-group-item"
+                                v-for="(tag, index) in getUniqueTags()"
+                                v-bind:key="index"
+                                @click="selectTag(tag)">
+                                {{  tag.name || 'None' }} ({{ tag.count }})
+
+                                <span class="pull-right">
+                                    <span class="btn btn-xs btn-default" @click="selectTag(tag)">
+                                        <i class="fa" :class="isCurrentTag(tag) ? 'fa-check-square-o' : 'fa-square-o'"></i>
+                                    </span>
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import '../assets/right_sidebar.css'
+
     import { onBeforeMount } from 'vue'
     import {
         useUiStore,
@@ -72,7 +81,6 @@
                 const tags = this.updateTags(tag);
 
                 this.$router.push({name: 'Home', query: { tags: tags }});
-                //this.toggleMenu()
             },
 
             updateTags: function (tag) {
@@ -103,76 +111,3 @@
 		}
     }
 </script>
-
-<style scoped>
-    #tags-sidebar-wrapper {
-        position: absolute;
-        width: 0;
-        right: 0;
-        height: 100%;
-        overflow-y: hidden;
-        transition: all .5s;
-        display: flex;
-        align-items: center;
-        z-index: 9999;
-    }
-
-    .tags-sidebar {
-        transition: all .5s;
-        overflow-y: auto;
-        height: 100%;
-        width: 100%;
-        padding: 1rem;
-        border-bottom: 1px solid #e9ecef;
-        border-top-left-radius: 0.3rem;
-        border-top-right-radius: 0.3rem;
-        backdrop-filter: blur(18px) contrast(60%);
-    }
-
-    .sidebar-header .justify-content-end {
-        display: flex;
-    }
-
-   .sidebar-header .close {
-        font-size: 2.5rem;
-    }
-
-	@media (prefers-color-scheme: dark) {
-		.sidebar-header .close {
-			color: #e4e6eb;
-		}
-	}
-
-    .tags-sidebar {
-      border: none;
-      background: #ffffff;
-    }
-	
-	@media (prefers-color-scheme: dark) {
-		.tags-sidebar {
-			color: #e4e6eb;
-			background: var(--color-background-soft);
-		}
-	}
-	
-	@media only screen and (min-width: 500px) {
-        .tags-sidebar {
-			background: #fff;
-            border-top-left-radius: 15px;
-            border-top-right-radius: 0;
-            border-bottom-left-radius: 15px;
-            border-bottom-right-radius: 0;
-            box-shadow: 0 0 2rem rgba(0,0,255,.1);
-        }
-		
-		@media (prefers-color-scheme: dark) {
-			.tags-sidebar {
-				background: var(--color-background-mute);
-			}
-		}
-    }
-
-    .list-group-item {
-        color: var(--color-text);
-	}
-</style>
