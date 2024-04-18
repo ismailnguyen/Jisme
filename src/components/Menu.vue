9f65f0baf3bdd3850cba3c3d12086b0c17b84f15
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-04-18 17:06:04 +0800

    refacto

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -4,7 +4,7 @@
             <img :src="user.avatarUrl" class="rounded-circle mb-3" style="width: 150px;" alt="Avatar" />
             <p class="text-muted">{{ user.email }}</p>
 
-            <div class="d-grid gap-2 d-none d-lg-grid">
+            <div class="d-grid gap-2 d-none d-md-grid">
                 <button class="menu-actions-add btn btn-danger" type="button" @click="onAddAccount()">
                     <i class="fa fa-plus"></i>
                     Add account
@@ -21,7 +21,7 @@
                 </button>
             </div>
 
-            <li class="menu-actions d-lg-none">
+            <li class="menu-actions d-md-none">
                 <a class="menu-actions-settings" @click="onOpenSettings()" alt="Settings" title="Settings">
                     <i class="fa fa-gear"></i>
                 </a>
@@ -39,7 +39,7 @@
 </template>
 
 <script>
-    import '../assets/menubar.css'
+    import '../assets/left_sidebar.css'
 
     import {
         mapState,
