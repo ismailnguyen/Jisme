4632cabfd0c6b16955607397c02cc8bf7e5a8ee2
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-04-18 16:55:53 +0800

    expand menu actions on large screens

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,7 +3,25 @@
         <ul class="sidebar-nav">
             <img :src="user.avatarUrl" class="rounded-circle mb-3" style="width: 150px;" alt="Avatar" />
             <p class="text-muted">{{ user.email }}</p>
-            <li class="menu-actions">
+
+            <div class="d-grid gap-2 d-none d-lg-grid">
+                <button class="menu-actions-add btn btn-danger" type="button" @click="onAddAccount()">
+                    <i class="fa fa-plus"></i>
+                    Add account
+                </button>
+
+                <button class="menu-actions-tags btn btn-primary" type="button" @click="onOpenTags()">
+                    <i class="fa fa-tags"></i>
+                    Tags
+                </button>
+
+                <button class="menu-actions-settings btn btn-primary" type="button" @click="onOpenSettings()">
+                     <i class="fa fa-gear"></i>
+                     Settings
+                </button>
+            </div>
+
+            <li class="menu-actions d-lg-none">
                 <a class="menu-actions-settings" @click="onOpenSettings()" alt="Settings" title="Settings">
                     <i class="fa fa-gear"></i>
                 </a>
