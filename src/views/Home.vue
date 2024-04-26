a08178d9a30567faca46676768d48c392b3cea1d
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-04-26 17:25:52 +0800

    move nullity check on top level

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="sidebarAdjustmentClasses">
+    <div :class="sidebarAdjustmentClasses" v-if="user">
         <Menu :visible="isMenuOpened" />
  
         <AddAccountModal :visible="isAddAccountModalOpened" />
