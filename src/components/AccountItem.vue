632d29660fa1579dffa4260e5d861acde9db52bf
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-06-13 18:14:24 +0800

    on account item click do not expand but open bottom sheet with small view

--- src/components/AccountItem.vue
+++ src/components/AccountItem.vue
@@ -30,7 +30,6 @@
                 <div class="row" v-if="(account.type == 'account' || account.type == '2fa') && account.login">
                     <div class="col-sm-12">
                         <span class="small">
-                            <i class="fa fa-id-badge" aria-hidden="true" v-show="isExpanded"></i>
                             {{ account.login }}
                         </span>
                     </div>
@@ -45,76 +44,13 @@
                     </div>
                 </div>
 
-                <div class="row" v-show="isExpanded" v-if="(account.type == 'account' || account.type == '2fa') && (account.password || account.password_clue)">
-                    <div class="col-sm-12">
-                        <span class="small" v-if="account.password">
-                            <i class="fa fa-unlock" aria-hidden="true"></i>
-                            {{ account.password }}
-                        </span>
-                        <span class="small" v-if="account.password_clue && !account.is_password_less">
-                            <i class="fa fa-eye" aria-hidden="true"></i>
-                            {{ account.password_clue }}
-                        </span>
-                        <span class="small" v-if="account.is_password_less">
-                            <i class="fa fa-bolt" aria-hidden="true"></i>
-                            Password less
-                        </span>
-                    </div>
-                </div>
-
-                <div class="row" v-show="isExpanded" v-if="account.social_login">
-                    <div class="col-sm-12">
-                        <span class="small">
-                            <i class="fa fa-mobile-screen" aria-hidden="true"></i>
-                            &nbsp;
-                            <div
-                                class="badge rounded-pill text-bg-danger"
-                                v-for="socialLogin in account.social_login.split(',')"
-                                v-show="account.social_login"
-                                v-bind:key="socialLogin">
-                                {{ socialLogin }}
-                            </div>
-                        </span>
-                    </div>
-                </div>
-
-                <div class="row" v-show="isExpanded" v-if="account.type == 'card' && account.card_number">
-                    <div class="col-sm-12">
-                        <span class="small">
-                            <i class="fa fa-barcode" aria-hidden="true"></i>
-                            {{ account.card_number }}
-                        </span>
-                    </div>
-                </div>
-
-                <div class="row" v-show="isExpanded" v-if="account.totp_secret">
-                    <div class="col-sm-12">
-                        <span class="small">
-                            <i class="fa fa-qrcode" aria-hidden="true"></i>
-                            {{ totpToken }}
-                        </span>
-                    </div>
-                </div>
-
                 <div class="row" v-if="account.description">
                     <div class="col-sm-12">
-                        <span class="small description" v-show="!isExpanded">
+                        <span class="small description">
                             {{ shortDescription }}
-                        </span>
-
-                        <span class="small description" v-show="isExpanded">
-                            <i class="fa fa-message" aria-hidden="true"></i>
-                            {{ account.description }}
                         </span>
                     </div>
                 </div>
-            </div>
-
-            <div class="card-footer" v-show="isExpanded">
-                <a class="btn btn-outline-primary float-end" @click.prevent="edit()">
-                    Open
-                    <i class="fa fa-arrow-right"></i>
-                </a>
             </div>
         </div>
     </div>
@@ -140,7 +76,6 @@
     } from '@/store'
     import Account from '../models/Account'
     import { truncateString } from '../utils/textFormat'
-    import totpGenerator from 'totp-generator'
 
     export default {
         props: {
@@ -150,15 +85,8 @@
                 default: 'small'
             }
         },
-        data() {
-            return {
-                isExpanded: false,
-                totpToken: ''
-            }
-        },
         computed: {
             ...mapState(useUiStore, [
-                'isLeftSidebarOpened',
                 'isRightSidebarOpened',
                 'SIDEBAR'
             ]),
@@ -203,22 +131,8 @@
                 this.openSidebar(this.SIDEBAR.EDIT_ACCOUNT);
             },
 
-            onCardClick: function() {
-                // Expand or collapse the card on click on it
-                this.isExpanded = !this.isExpanded;
-
-                // Regenerate totp token if the card is expanded
-                if (this.isExpanded && this.account.totp_secret) {
-                    this.generateTotpToken();
-                }
-            },
-
-            generateTotpToken: function () {
-                if (this.account.totp_secret) {
-                     // Remove all spaces because spaces are forbidden for TOTP generation
-                     // And some websites give the secret with spaces for better human readability
-                    this.totpToken = totpGenerator(this.account.totp_secret.replace(/ /g,''));
-                }
+            onCardClick: async function() {
+                await this.edit();
             },
 
             selectTag: function (tag) {
