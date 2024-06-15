a92d070705c3588d06cf4e13350dc3ec9b95bca5
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-06-16 01:17:42 +0800

    fix searchbar on top scroll

--- src/components/AccountList.vue
+++ src/components/AccountList.vue
@@ -11,28 +11,26 @@
     </div>
 
     <div id="page-content-wrapper" class="container-fluid">
-        <header class="row search-input-container justify-content-center">
-            <div class="col-xs-12 col-md-8 col-lg-6">
-                <div
-                class="input-group mb-3"
-              >
-              <button
-                  class="btn search-left-btn"
-                  type="button"
-                  @click="onMenuOpened"
-                >
-                  <i class="fa fa-solid fa-bars-staggered"></i>
-                </button>
-                <input
-                  class="form-control search-input" 
-                  type="search"
-                  ref="searchInput"
-                  v-model="searchQuery"
-                  autofocus
-                  placeholder="Search"
-                  :disabled="isLoading"
-                />
-              </div>
+        <header class="search-input-container">
+            <div
+            class="input-group mb-3"
+            >
+            <button
+                class="btn search-left-btn"
+                type="button"
+                @click="onMenuOpened"
+            >
+                <i class="fa fa-solid fa-bars-staggered"></i>
+            </button>
+            <input
+                class="form-control search-input" 
+                type="search"
+                ref="searchInput"
+                v-model="searchQuery"
+                autofocus
+                placeholder="Search"
+                :disabled="isLoading"
+            />
             </div>
         </header>
 
@@ -43,14 +41,12 @@
             v-if="!isSearching" />
 
         <RecentAccountList
-            :accountsCardSize="accountsCardSize"
             :isLoading="isLoading"
             v-if="!isSearching" />
 
         <FilteredAccountList
             :searchQuery="searchQuery"
             :filteredAccounts="filteredAccounts"
-            :accountsCardSize="accountsCardSize"
             :isLoading="isLoading"
             v-if="isSearching" />
         
@@ -67,7 +63,6 @@
     import {
         useAccountsStore,
         useAlertStore,
-        useUiStore,
      } from '@/store'
     import LoadingAccountItem from '../components/LoadingAccountItem.vue'
     import AccountItem from '../components/AccountItem.vue'
@@ -97,7 +92,8 @@
                     isFetching: false,
                     totalFetched: 0,
                     total: 0
-                }
+                },
+                lastScrollTop: 0
             }
         },
         async created() {
@@ -164,9 +160,6 @@
                 'totalFetchedAccounts',
                 'totalAccounts',
             ]),
-            ...mapState(useUiStore, [
-                'isRightSidebarOpened',
-            ]),
 
             isSearching: function () {
                 return (this.searchQuery 
@@ -174,14 +167,6 @@
                             || this.selectedTags.length > 0
                             || this.selectedTypes.length > 0;
             },
-
-            accountsCardSize: function () {
-                if (this.isRightSidebarOpened) {
-                    return 'medium';
-                }
-
-                return 'small';
-            }
         },
         methods: {
             ...mapActions(useAccountsStore, [
@@ -201,15 +186,15 @@
 
             hideSearchBarOnScroll: function (event) {
                 const scrollingElement = event.target.scrollingElement;
-                var lastScrollTop = 0;
 
                 var st = scrollingElement.scrollTop;
 
-                var isScrollDown = st > lastScrollTop;
+                var isScrollDown = st > this.lastScrollTop;
 
-                document.querySelector('.search-input-container').style.bottom = isScrollDown ? '-8rem' : '0.2rem';
+                // document.querySelector('.search-input-container').style.bottom = isScrollDown ? '-8rem' : '0.2rem';
+                document.querySelector('.search-input-container').style.display = isScrollDown ? 'none' : 'block';
 
-                lastScrollTop = st;
+                this.lastScrollTop = st;
             },
 
             onAccountsLoaded: function (mutation, state) {
@@ -270,30 +255,40 @@
 </script>
 
 <style>
-    /* Reduce search bar size when right sidebar is opened */
-    .right-sidebar-opened .search-input-container {
-        padding-right: 50%;
-    }
+    .search-input-container {
+            color: var(--color-text);
+            padding: 20px 20px 20px 40px;
+            z-index: 10;
+            position: fixed;
+            left: 0;
+            right: 0;
+            width: 60%;
+            margin: auto;
+            transition: all .5s;
+        }
 
-    .search-input-container .input-group {
-        box-shadow: 2px 4px 12px rgb(0 0 0 / 20%);
-        border-radius: 1.1rem;
+    @media (min-width: 767.98px) { 
+        .search-input-container {
+            top: 0.2rem;
+        }
     }
 
     @media (max-width: 767.98px) { 
         .search-input-container {
-            position: fixed;
             bottom: 0.2rem;
-            left: 0;
-            right: 0;
-            transition: all .5s;
+            width: 100%;
         }
+    }
+
+    .search-input-container .input-group {
+        box-shadow: 2px 4px 12px rgb(0 0 0 / 20%);
+        border-radius: var(--btn-border-radius);
     }
 
     .search-left-btn {
         color: #FFF;
         line-height: 2.5;
-        background-color: #ced4da70;
+        background-color: var(--btn-color-background);
         background: linear-gradient(81.62deg, #2870ea 8.72%, #1b4aef 85.01%);
         backdrop-filter: blur(18px);
     }
@@ -305,12 +300,24 @@
 		box-shadow: 2px 4px 16px rgb(0 0 0 / 16%);
         transform: scale3d(1.01,1.01,1.01);
 	}
+
+    @media (prefers-color-scheme: dark) {
+		.search-left-btn {
+            background: RGBA(var(--bs-primary-rgb), var(--bs-bg-opacity, 1));
+		}
+
+        .search-left-btn:hover,
+        .search-left-btn:active,
+        .search-left-btn:focus {
+            background: linear-gradient(81.62deg, #2870ea 8.72%, #1b4aef 85.01%);
+        }
+	}
     
     .search-input {
-        border-radius: 1.1rem;
+        border-radius: var(--btn-border-radius);
         color: #000;
         line-height: 2.5;
-        background-color: #ced4da70;
+        background-color: var(--btn-color-background);
         backdrop-filter: blur(18px);
     }
 
@@ -336,7 +343,7 @@
     .search-input:hover,
     .search-input:active,
     .search-input:focus {
-        background-color: #ced4da70;
+        background-color: var(--btn-color-background);
 		box-shadow: 2px 4px 16px rgb(0 0 0 / 16%);
         transform: scale3d(1.01,1.01,1.01);
 	}
