ba67f90f6f52d7759e27d12dc249356211199009
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-06-21 17:12:54 +0800

    align loading card item design

--- src/components/LoadingAccountItem.vue
+++ src/components/LoadingAccountItem.vue
@@ -1,30 +1,30 @@
 <template>
-    <div class="card-wrapper col-12 col-xs-12 col-sm-12 col-md-4 col-lg-3">
+<div class="card-wrapper col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
         <div class="card clickable">
-            <div class="card-image-wrapper col-xs-3">
-                <img
-                    :src="getIcon"
-                    loading="lazy"
-                    class="card-icon" />
-            </div>
-
-            <div class="card-header tags placeholder-glow">
-                <span class="placeholder col-4 bg-primary"></span>
-            </div>
-
             <div class="card-body">
-                <h2 class="card-title placeholder-glow">
-                    <span class="placeholder col-6"></span>
-                </h2>
-
                 <div class="row">
-                    <div class="col-sm-12">
-                        <span class="placeholder col-4"></span>
+                    <div class="col-12 tags placeholder-glow">
+                        <span class="placeholder col-4 bg-primary"></span>
                     </div>
                 </div>
 
                 <div class="row">
-                    <div class="col-sm-12">
+                    <div class="col-8 align-self-center">
+                        <h2 class="card-title placeholder-glow">
+                            <span class="placeholder col-6"></span>
+                        </h2>
+                    </div>
+
+                    <div class="col-4 align-self-center">
+                        <img
+                        :src="getIcon"
+                        loading="lazy"
+                        class="card-icon float-end" />
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12">
                         <span class="placeholder col-8"></span>
                     </div>
                 </div>
