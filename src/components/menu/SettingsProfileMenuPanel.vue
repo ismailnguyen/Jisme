1fa6f3446ebaa126b0397a500b1923142f4bba84
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-07-02 18:03:59 +0800

    Redesign settings profile panel with accordion

--- src/components/menu/SettingsProfileMenuPanel.vue
+++ src/components/menu/SettingsProfileMenuPanel.vue
@@ -2,31 +2,64 @@
   <div class="row">
     <Loader v-show="isLoading" />
 
-    <div class="mb-3 col-xs-12 col-md-12 col-lg-12">
-      <label class="form-label" for="inputEmail"
-        ><i class="fa fa-id-badge" aria-hidden="true"></i> Username</label
-      >
-      <input
-        id="inputEmail"
-        class="form-control"
-        placeholder="Username, email address, phone number"
-        type="email"
-        v-model="user.email"
-        disabled
-      />
-    </div>
+    <div class="accordion">
+      <div class="accordion-item">
+        <h2 class="accordion-header ">
+          <button
+            class="accordion-button" :class="fieldAttrs.username.isExpanded ? '' : 'collapsed'"
+            type="button"
+            @click="fieldAttrs.username.isExpanded = !fieldAttrs.username.isExpanded">
+            <div>
+              <div class="fw-medium">
+                <i class="fa fa-id-badge" aria-hidden="true"></i>
+                Username
+              </div>
+              <span class="fw-lighter" v-show="!fieldAttrs.username.isExpanded">
+                {{ user.email }}
+              </span>
+            </div>
+          </button>
+        </h2>
+        <div class="accordion-collapse" :class="fieldAttrs.username.isExpanded ? 'show' : 'collapse'">
+          <div class="accordion-body">
+              <input
+                id="inputEmail"
+                class="form-control"
+                placeholder="Username, email address, phone number"
+                type="email"
+                v-model="user.email"
+                disabled
+              />
+          </div>
+        </div>
+      </div>
 
-    <div class="mb-3 col-xs-12 col-md-12 col-lg-12">
-      <label class="form-label" for="inputAvatar"
-        ><i class="fa fa-circle-user" aria-hidden="true"></i> Avatar</label
-      >
-      <input
-        id="inputAvatar"
-        class="form-control"
-        type="text"
-        laceholder="Avatar image URL"
-        v-model="user.avatarUrl"
-      />
+      <div class="accordion-item">
+        <h2 class="accordion-header ">
+          <button
+            class="accordion-button" :class="fieldAttrs.avatar.isExpanded ? '' : 'collapsed'"
+            type="button"
+            @click="fieldAttrs.avatar.isExpanded = !fieldAttrs.avatar.isExpanded">
+            <div>
+              <div class="fw-medium">
+                <i class="fa fa-circle-user" aria-hidden="true"></i>
+                Avatar
+              </div>
+            </div>
+          </button>
+        </h2>
+        <div class="accordion-collapse" :class="fieldAttrs.avatar.isExpanded ? 'show' : 'collapse'">
+          <div class="accordion-body">
+              <input
+                id="inputAvatar"
+                class="form-control"
+                type="text"
+                laceholder="Avatar image URL"
+                v-model="user.avatarUrl"
+              />
+          </div>
+        </div>
+      </div>
     </div>
 
     <hr class="my-4" />
@@ -63,6 +96,14 @@ export default {
   data() {
     return {
       isLoading: false,
+      fieldAttrs: {
+        username: {
+          isExpanded: false,
+        },
+        avatar: {
+          isExpanded: false,
+        }
+      },
     };
   },
   computed: {
