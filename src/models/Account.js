0be47a8a74910ad4fd79963a4b39bba55fc8c394
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2025-06-04 23:57:00 +0200

    make account constructor flexible

--- src/models/Account.js
+++ src/models/Account.js
@@ -3,66 +3,59 @@ import { randomPassword } from '../utils/password'
 import { generatePassword } from "../utils/passwordLess";
 
 class Account {
-    constructor (
-            _id = 0, 
-            label = '',
-            platform = '',
-            icon = '',
-            login = '', 
-            password = '',
-            is_password_less = true, // By default enforce password less
-            password_clue = '', 
-            tags = '', 
-            created_date = new Date(),
-            social_login = '', 
-            description = '',
-            notes = '',
-            last_modified_date = null, 
-            last_opened_date = null, 
-            opened_count = 0,
-            type = 'account',
-            subtype = 'login',
-            card_number = '',
-            card_name = '',
-            card_expiracy = '',
-            card_cryptogram = '',
-            card_pin = '',
-            card_format = '',
-            totp_secret = '',
-            isServerEncrypted = false
-        ) {
-        this._id = _id;
-        this.icon = icon;
-        this.label = label;
-        this.platform = platform;
-        this.login = login;
-        this.password = password;
-        this.is_password_less = Boolean(is_password_less);
-		this.password_clue = password_clue;
-        this.tags = tags;
-        this.created_date = new Date(created_date).toUTCString();
-		this.social_login = social_login;
-		this.description = description;
-		this.notes = notes;
+    constructor (account = {}) {
+        if (!account)
+            return;
+
+        this._id = account.id || 0;
+        this.icon = account.icon || '';
+        this.label = account.label || '';
+        this.platform = account.platform || '';
+        this.login = account.login || '';
+        this.password = account.password || '';
+        this.is_password_less = typeof account.is_password_less == "boolean" ? Boolean(account.is_password_less) : true; // By default enforce password less
+		this.password_clue = account.password_clue || '';
+        this.tags = account.tags || '';
+        this.created_date = account.created_date ? new Date(account.created_date).toUTCString() : new Date().toUTCString();
+		this.social_login = account.social_login || '';
+		this.description = account.description || '';
+		this.notes = account.notes || '';
         // if account never modified, take creation date as last modification date
-        this.last_modified_date = new Date((last_modified_date instanceof Date && !isNaN(last_modified_date)) ? last_modified_date : created_date).toUTCString();
+        this.last_modified_date = new Date((account.last_modified_date instanceof Date && !isNaN(account.last_modified_date)) ? account.last_modified_date : account.created_date).toUTCString();
         // if account never opened, take creation date as last opened date
-        this.last_opened_date = new Date((last_opened_date instanceof Date && !isNaN(last_opened_date)) ? last_opened_date : created_date).toUTCString();
-        this.opened_count = opened_count;
-        this.subtype = subtype;
-        this.type = type;
-        this.card_number = card_number;
-        this.card_name = card_name;
-        this.card_expiracy = card_expiracy;
-        this.card_cryptogram = card_cryptogram;
-        this.card_pin = card_pin;
-        this.card_format = card_format;
-        this.totp_secret = totp_secret;
-        this.isServerEncrypted = isServerEncrypted;
+        this.last_opened_date = new Date((account.last_opened_date instanceof Date && !isNaN(account.last_opened_date)) ? account.last_opened_date : account.created_date).toUTCString();
+        this.opened_count = account.opened_count || 0;
+        this.type = account.type || 'account';
+        this.subtype = account.subtype || 'login';
+        this.card_number = account.card_number || '';
+        this.card_name = account.card_name || '';
+        this.card_expiracy = account.card_expiracy || '';
+        this.card_issue_date = account.card_issue_date || '';
+        this.card_cryptogram = account.card_cryptogram || '';
+        this.card_pin = account.card_pin || '';
+        this.card_format = account.card_format || '';
+        this.totp_secret = account.totp_secret || '';
+        this.isServerEncrypted = account.   isServerEncrypted || false;
     }
 
     get displayPlatform () {
         return cleanUrl(this.platform);
+    }
+
+    get displayType () {
+        // This workaround is needed to avoid confusion with the Account model
+        if (this.type === 'account') { 
+            return 'Credential';
+        }
+        return this.type.charAt(0).toUpperCase() + this.type.slice(1);
+    }
+
+    get displaySubtype () {
+        if (['iban', 'swift'].includes(this.subtype)) { 
+            return this.subtype.toUpperCase();
+        }
+        
+        return this.subtype.charAt(0).toUpperCase() + this.subtype.slice(1);
     }
 
     generatePassword () {
@@ -84,11 +77,12 @@ class Account {
     }
 
     isValid () {
-        return this.platform !== '';
+        return this.label !== '';
     }
 
     contains (word) {
-        return this.platform.toLowerCase().includes(word) ||
+        return this.label.toLowerCase().includes(word) ||
+            this.platform.toLowerCase().includes(word) ||
             this.displayPlatform.toLowerCase().includes(word) ||
             this.login.toLowerCase().includes(word) ||
             this.social_login.toLowerCase().includes(word) ||
@@ -98,6 +92,7 @@ class Account {
             this.icon.toLowerCase().includes(word) ||
             this.card_number.toLowerCase().includes(word) ||
             this.card_expiracy.toLowerCase().includes(word) ||
+            this.card_issue_date.toLowerCase().includes(word) ||
             this.card_pin.toLowerCase().includes(word) ||
             this.card_cryptogram.toLowerCase().includes(word) ||
             this.card_name.toLowerCase().includes(word) ||
